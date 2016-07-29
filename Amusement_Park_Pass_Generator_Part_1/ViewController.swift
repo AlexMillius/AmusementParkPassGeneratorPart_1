22cd9215523649e1173bda31f83f083a745fb9d0
AlexMillius <alexmillius@gmail.com>
2016-07-29 15:58:03 +0200

    Initial Commit

---
+++ Amusement_Park_Pass_Generator_Part_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amusement_Park_Pass_Generator_Part_1
+//
+//  Created by Alex Millius on 29.07.16.
+//  Copyright © 2016 TumTum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
