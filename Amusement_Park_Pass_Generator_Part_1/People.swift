1e968fe335bea7a35a9e40ebac79e488623fd75e
AlexMillius <alexmillius@gmail.com>
2016-07-29 16:01:10 +0200

    Initial commit with sounds assets

---
+++ Amusement_Park_Pass_Generator_Part_1/People.swift
@@ -0,0 +1,9 @@
+//
+//  People.swift
+//  Amusement_Park_Pass_Generator_Part_1
+//
+//  Created by Mohamed Lee on 29.07.16.
+//  Copyright © 2016 TumTum. All rights reserved.
+//
+
+import Foundation
